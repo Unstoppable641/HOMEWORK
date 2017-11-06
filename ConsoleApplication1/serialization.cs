c29601503b9ec63be1ce30e7d53e4aeda9b01489
Никита <@NKTA-PC>
2017-11-06 12:55:38 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/serialization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Izhevsk = new Address();
+            var serialized = JsonConvert.SerializeObject(Izhevsk);
+            using (var streamWriter = new StreamWriter(new FileStream("out.txt", FileMode.OpenOrCreate)))
+            {
+                streamWriter.WriteLine(Izhevsk);
+            }
+        }
+        private class Address
+        {
+            public string Street { get; set; } = "79 Barishnikova St";
+            public string City { get; set; } = "Izhevsk";
+            public string State { get; set; } = "RF";
+            public string Zip { get; set; } = "426000";
+        }
+    }
+}
---
+++ Deserialization/deserialization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Deserialization
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+           var Izhevsk = new Address();
+            using (var streamReader = new StreamReader(new FileStream("out.txt", FileMode.OpenOrCreate)))
+            {
+                var serializ = streamReader.ReadLine();
+                var deserialization = JsonConvert.DeserializeObject<Address>(serializ);
+                Console.WriteLine(deserialization);
+                Console.ReadKey();
+            }
+        }
+        private class Address
+        {
+            public string Street { get; set; }
+            public string City { get; set; }  
+            public string State { get; set; } 
+            public string Zip { get; set; }                 
+        }
+    }
+}
